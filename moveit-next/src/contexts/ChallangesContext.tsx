4d5eab57937fff5401170b8d400bb2add5d31689
Guilherme Fernandes <guilherme.fernandes01@outlook.com>
2021-02-28 20:09:16 -0300

    Context

---
+++ moveit-next/src/contexts/ChallangesContext.tsx
@@ -0,0 +1,64 @@
+import { createContext, ReactNode, useState } from 'react';
+import challanges from '../../challenges.json';
+
+interface Challange {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
+
+interface ChallangesContextData {
+    level: number;
+    currentExperience: number;
+    challangesCompleted: number;
+    activeChallange: Challange;
+    experienceToNextLevel: number;
+    levelUp: () => void;
+    startNewChallange: () => void;
+    resetChallange: () => void;
+}
+
+interface ChallangesProviderProps {
+    children: ReactNode;
+}
+
+export const ChallangesContext = createContext({} as ChallangesContextData);
+
+export function ChallangesProvider({ children }: ChallangesProviderProps) {
+    const [level, setLevel] = useState(1);
+    const [currentExperience, setCurrentExperience] = useState(0);
+    const [challangesCompleted, setChallangesCompleted] = useState(0);
+    const [activeChallange, setActiveChallange] = useState(null);
+
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
+    function levelUp() {
+        setLevel(level + 1);
+    }
+
+    function startNewChallange() {
+        const randomChallangeIndex = Math.floor(Math.random() * challanges.length);
+        const challange = challanges[randomChallangeIndex];
+
+        setActiveChallange(challange);
+    }
+
+    function resetChallange() {
+        setActiveChallange(null);
+    }
+
+    return (
+        <ChallangesContext.Provider value={{
+            level,
+            currentExperience,
+            challangesCompleted,
+            activeChallange,
+            experienceToNextLevel,
+            levelUp,
+            startNewChallange,
+            resetChallange,
+        }}>
+            { children }
+        </ChallangesContext.Provider>
+    );
+}
